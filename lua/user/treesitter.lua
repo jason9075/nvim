3a0977a9d31bad82b245543e318c08b54813cf36
jason9075@gmail.com <jason9075@gmail.com>
2022-04-28 13:40:46 +0800

    fix: assign treesitter parser languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,23 +1,28 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+    return
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-		enable = true,
-	},
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = { "css", "scss", "regex", "go", "swift", "rst", "typescript",
+        "json", "solidity", "scala", "lua", "http", "hjson", "toml", "cuda", "comment",
+        "vim", "godot_resource", "jsdoc", "dockerfile", "turtle", "jsonc", "query",
+        "yaml", "python", "r", "cpp", "gomod", "bash", "cmake", "html", "tsx", "help",
+        "json5", "fish", "latex", "java", "make", "llvm", "kotlin", "markdown" },
+    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    autopairs = {
+        enable = true,
+    },
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+    },
+    indent = { enable = true, disable = { "yaml" } },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
 }
